66c7da23ca26e1264f300b0d39502120b8b24f55
Michael Buhot <m.buhot@gmail.com>
2017-08-18 20:29:57 +1000

    Add tests for Worker module

--- lib/ecto_job/worker.ex
+++ lib/ecto_job/worker.ex
@@ -7,13 +7,18 @@ defmodule EctoJob.Worker do
   @type repo :: module
 
   @doc """
+  Equivalent to `start_link(repo, job, DateTime.utc_now())`
+  """
+  @spec start_link(repo, EctoJob.JobQueue.job) :: {:ok, pid} | {:error, term}
+  def start_link(repo, job), do: start_link(repo, job, DateTime.utc_now())
+
+  @doc """
   Start a worker process given a repo module and a job struct
   This may fail if the job reservation has expired, in which case the job will be
   reactivated by the producer.
   """
-  @spec start_link(repo, EctoJob.JobQueue.job) :: {:ok, pid} | {:error, term}
-  def start_link(repo, job) do
-    now = DateTime.utc_now()
+  @spec start_link(repo, EctoJob.JobQueue.job, DateTime.t) :: {:ok, pid} | {:error, term}
+  def start_link(repo, job, now) do
     with {:ok, job} <- JobQueue.update_job_in_progress(repo, job, now),
          {:ok, {mod, func, args}} <- JobQueue.deserialize_job_args(job) do
       Task.start_link(mod, func, args)
